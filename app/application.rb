ff535f7381dd59abd818370a19d8967b857ac7b1
Nathan Brickett <natey37@gmail.com>
2020-01-27 11:06:19 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,14 @@
+class Application
+
+    def call(env)
+        resp = Rack::Response.new
+        time = Time.now 
+
+        if time.hour >= 12 
+            resp.write "Good afternoon"
+        else 
+            resp.write "Good morning"
+        end 
+        resp.finish
+    end 
+end 
